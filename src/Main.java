58404a2f7ef8cf349340f5816941ed1db96ee6a9
Gaoussou Doucoure <gaousd4@gmail.com>
2025-05-22 10:19:07 -0700

    Overloading Constructors and Methods

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,40 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+class Person{
+    String firstName;
+    String lastName;
+    int age;
+
+    Person(String firstName){
+        this.firstName = firstName;
+    }
+    Person(String firstName, String lastName){  //Overloading previous constructor
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+    Person(String firstName, String lastName, int age){  //Overloading both previous constructors
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    public int add(int i, int j){
+        int result = i + j;
+        System.out.println(result);
+        return result;
+    }
+    public int add(int i, int j, int k){   //Overloading method
+        int result = i + j;
+        System.out.println(result);
+        return result;
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Person p1 = new Person("Gaoussou");
+        Person p2 = new Person("Gaoussou", "Doucoure");
+        Person p3 = new Person("Gaoussou", "Doucoure", 00);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        p1.add(2,3);
+
     }
 }
